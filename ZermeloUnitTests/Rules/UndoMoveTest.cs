8d167832fcf3a977f975d63d9ee6fe7ecdd8dc73
Alexey <alexey.khrenov@yandex.ru>
2020-04-28 20:05:25 +0300

    Added tests for Undo move

--- ZermeloUnitTests/Rules/UndoMoveTest.cs
+++ ZermeloUnitTests/Rules/UndoMoveTest.cs
@@ -1,4 +1,5 @@
-﻿using Game.Implementations;
+﻿using Checkers;
+using Game.Implementations;
 using Game.Primitives;
 using System;
 using System.Collections.Generic;
@@ -42,6 +43,120 @@ namespace ZermeloUnitTests.Rules
 
             // ACT
             _rules.Undo(sourceBoard, move, null);
+
+            // ASSERT
+            AssertBoardsAreEqual(targetBoard, sourceBoard);
+        }
+
+        [Fact]
+        public void UndoMovesTest_2()
+        {
+            // ARRANGE
+            var sourceBoardStr = new string[]
+            {
+                "___b",
+                "w___",
+                "____",
+                "w___"
+            };
+            var sourceBoard = new BoardMock(sourceBoardStr, 4, false);
+            sourceBoard.SwitchPlayers();
+
+            var targetBoardStr = new string[]
+            {
+                "___b",
+                "____",
+                "_b__",
+                "w_w_"
+            };
+            var targetBoard = new BoardMock(targetBoardStr, 4, false);
+            targetBoard.Player1.Figures.First(f => f.X == 0 && f.Y == 3).AvailableMoves =
+                new List<Cell>() { new Cell(2, 1) };
+            targetBoard.Player1.Figures.First(f => f.X == 2 && f.Y == 3).AvailableMoves =
+                new List<Cell>() { new Cell(0, 1) };
+
+            var move = new HistoryItem(sourceBoard.AwaitingPlayer, new Move(new Cell(2, 3), new Cell(0, 1)));
+            move.Captured = new Piece(1, 2, false, false, true);
+
+            // ACT
+            _rules.Undo(sourceBoard, move, null);
+
+            // ASSERT
+            AssertBoardsAreEqual(targetBoard, sourceBoard);
+        }
+
+        [Fact]
+        public void UndoMovesTest_3()
+        {
+            // ARRANGE
+            var sourceBoardStr = new string[]
+            {
+                "___W",
+                "____",
+                "____",
+                "B_w_"
+            };
+            var sourceBoard = new BoardMock(sourceBoardStr, 4, false);
+            sourceBoard.SwitchPlayers();
+
+            var targetBoardStr = new string[]
+            {
+                "____",
+                "__B_",
+                "_w__",
+                "B_w_"
+            };
+            var targetBoard = new BoardMock(targetBoardStr, 4, false);
+            targetBoard.Player1.Figures.First(f => f.X == 1 && f.Y == 2).AvailableMoves =
+                new List<Cell>() { new Cell(3, 0) };
+
+            var move = new HistoryItem(sourceBoard.AwaitingPlayer, new Move(new Cell(1, 2), new Cell(3, 0)));
+            move.Captured = new Piece(2, 1, false, false, true, true);
+            move.IsPieceChangeType = true;
+
+            // ACT
+            _rules.Undo(sourceBoard, move, null);
+
+            // ASSERT
+            AssertBoardsAreEqual(targetBoard, sourceBoard);
+        }
+
+        [Fact]
+        public void UndoMovesTest_4()
+        {
+            // ARRANGE
+            var sourceBoardStr = new string[]
+            {
+                "__W__",
+                "_____",
+                "_____",
+                "_____",
+                "____B"
+            };
+            var sourceBoard = new BoardMock(sourceBoardStr, 5, false);
+            sourceBoard.SwitchPlayers();
+
+            var targetBoardStr = new string[]
+            {
+                "_____",
+                "_B___",
+                "w____",
+                "_____",
+                "____B"
+            };
+            var targetBoard = new BoardMock(targetBoardStr, 5, false);
+            targetBoard.Player1.Figures.First(f => f.X == 0 && f.Y == 2).AvailableMoves =
+                new List<Cell>() { new Cell(2, 0) };
+
+            var move = new HistoryItem(sourceBoard.AwaitingPlayer, new Move(0, 2, 2, 0));
+            move.Captured = new Piece(1, 1, false, false, true, true);
+            move.IsPieceChangeType = true;
+
+            var latestMoveBeforeUnto = new HistoryItem(sourceBoard.AwaitingPlayer, new Move(2, 4, 0, 2));
+            latestMoveBeforeUnto.Captured = new Piece(1, 3, false, false, true, true);
+
+            // ACT
+            _rules.Undo(sourceBoard, move, latestMoveBeforeUnto);
 
             // ASSERT
             AssertBoardsAreEqual(targetBoard, sourceBoard);
