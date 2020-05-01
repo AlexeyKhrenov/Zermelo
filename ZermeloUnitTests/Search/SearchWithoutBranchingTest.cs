3cde4606d7b88565aef537b9a654573cd2187bc2
Alexey <alexey.khrenov@yandex.ru>
2020-05-01 19:18:06 +0300

    Fixed SearchWithoutBranchingTest

--- ZermeloUnitTests/Search/SearchWithoutBranchingTest.cs
+++ ZermeloUnitTests/Search/SearchWithoutBranchingTest.cs
@@ -26,10 +26,10 @@ namespace ZermeloUnitTests.Search
         }
 
         [Theory]
-        [InlineData("1", 4, 0, 3)]
-        [InlineData("7 -2 -3", 3, 1, -2)]
-        [InlineData("1 -2 0 -6 8 1 0", 1, 2, 0)]
-        [InlineData("2 -1 -3 -4 0 7 2 1 1 13 -5 0 -6 -7 3", 9, 3, 5)]
+        [InlineData("1", 3, 0, 3)]
+        [InlineData("7 -2 -3", -4, 1, -2)]
+        [InlineData("1 -2 0 -6 8 1 0", 0, 2, 0)]
+        [InlineData("2 -1 -3 -4 0 7 2 1 1 13 -5 0 -6 -7 3", 7, 3, 5)]
         public void SearchWithoutBranchingTheory(string treeStr, sbyte expected, int depth, sbyte startState)
         {
             var tree = TreeGenerator.ParseByteTree(treeStr, 2);
